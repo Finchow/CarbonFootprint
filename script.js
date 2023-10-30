3b4be1295194f74b1ed51768c2a82567b6170fa4
Oscar White <oscar.white361@icloud.com>
2023-10-30 14:20:39 +0000

    Monthly switch feature

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const emissions = 0.207074288590604;
+var monthly = false;
 
-function clickHandler() {
-  clientUsage = document.getElementById(input).value;
+function clickHandler(monthly) {
+  clientUsage = document.getElementById("input").value;
   output = calcCO2(clientUsage, emissions);
   alert(`Your carbon output is: ${Math.round(output * 100) / 100} Kg per year`);
+}
+
+function monthlySwitch() {
+  function changeText(period) {
+    document.getElementById(
+      "top-text"
+    ).textContent = `Enter your estimated ${period} consumption (Kwh)`;
+    document.getElementById(
+      "monthly-text"
+    ).textContent = `Switch to ${period} consumption`;
+  }
+  monthly = !monthly;
+  switch (monthly) {
+    case true:
+      changeText("monthly");
+      break;
+    case false:
+      changeText("annual");
+      break;
+  }
 }
 
 function eraseInput() {
