9c9ad71d8dc7f7e796ed5866d78e1efa10d5fa2a
Oscar White <oscar.white361@icloud.com>
2023-11-03 14:13:06 +0000

    added people and house type estimate

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function monthlySwitch() {
   function changeText(period1, period2) {
     documentWriter(
       "top-text",
-      `Enter your estimated ${period1} consumption (Kwh)`
+      `Enter your estimated ${period1} consumption for electricity (Kwh)`
     );
     documentWriter("monthly-text", `Switch to ${period2} consumption`);
   }
@@ -56,15 +56,51 @@ function calcCO2(usage, emissions) {
   return usage * (0.85 * emissions);
 }
 
+function peopleCalculator() {
+  if (clientUsage < 1800) {
+    return "1 to 2";
+  } else if (clientUsage < 3200) {
+    return "2 to 3";
+  } else if (clientUsage < 4100 || clientUsage > 4100) {
+    return "4 to 5";
+  }
+}
+
+function houseCalculator() {
+  if (clientUsage < 2600) {
+    return "Mid terrace";
+  } else if (clientUsage < 3200) {
+    return "Flat";
+  } else if (clientUsage < 3500) {
+    return "End terrace";
+  } else if (clientUsage < 3800) {
+    return "Semi detached or Bungalow";
+  } else if (clientUsage < 4000 || clientUsage > 4000) {
+    return "Detached house";
+  }
+}
+
 function statWriter(totalCo2) {
+  documentWriter("usage", `Your EAC is ${clientUsage} kWh`);
   documentWriter(
-    "TotalOutput",
-    `Your carbon output from electricity is: ${totalCo2} Kg per year.`
+    "totalOutput",
+    `CO2 output from electricity is: ${totalCo2} Kg per year.`
   );
   documentWriter(
-    "Flights",
+    "people",
+    `You are estimated to have ${peopleCalculator()} people in your home.`
+  );
+  documentWriter("house", `You are estimated to live in ${houseCalculator()}.`);
+  documentWriter(
+    "flights",
     `Your electricity emissions are equivalent to ${Math.round(
       totalCo2 / 90
     )} hours of flight.`
+  );
+  documentWriter(
+    "miles",
+    `Your CO2 output equivalent to driving a petrol car for ${Math.round(
+      totalCo2 / 0.4
+    )} miles.`
   );
 }
